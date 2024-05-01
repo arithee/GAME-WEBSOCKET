1604f99680024405d75e8ac6d815bb34733456a1
arithee <trevoratienza53@gmail.com>
2024-05-01 21:44:57 +0800

    Functions for difficulty

--- game_server.py
+++ game_server.py
@@ -9,7 +9,7 @@ Select the difficulty level:
 a. Easy (1 - 50)
 b. Medium (1 - 100)
 c. Hard (1 - 500)
-Enter your guess:"""
+"""
 
 def generate_random_int(low, high):
     return random.randint(low, high)
@@ -33,25 +33,41 @@ print(f"server is listening in port {port}")
 
 while True:
     conn, addr = s.accept()
-    guessme = generate_random_int(1,100)
     print(f"new client: {addr[0]}")
     conn.sendall(banner.encode())
 
+    #Gets difficulty choice
+    difficulty_choice = conn.recv(1024).decode().strip().lower()
+    difficulty_range = get_difficulty_range(difficulty_choice)
+
+    if difficulty_range is None:
+        conn.sendall(b"Invalid difficulty choice. Please select again.")
+        conn.close()
+        continue
+
+    #This generates the number to guessed by the user using the difficulty range the client chose.
+    guessme = generate_random_int(*difficulty_range)
     while True:
         client_input = conn.recv(1024)
         guess = int(client_input.decode().strip())
         print(f"User guess attempt: {guess}")
         if guess == guessme:
-            conn.sendall(b"Correct Answer! Would you like to play again? (Y/N): ")
+            conn.sendall(b"Correct Answer!")
             play_again = conn.recv(1024).decode().strip().lower()
             if play_again != "Y":
                 conn.sendall(b"Goodbye!")
                 conn.close()
                 break
             else:
-                guessme = generate_random_int(1,100)
                 conn.sendall(banner.encode())
+                difficulty_choice = conn.recv(1024).decode().strip().lower()
+                difficulty_range = get_difficulty_range(difficulty_choice)
+                if difficulty_range is None:
+                    conn.sendall(b"Invalid difficulty choice. Please select again.")
+                    conn.close()
+                    break
+                guessme = generate_random_int(*difficulty_range)
         elif guess > guessme:
-            conn.sendall(b"Guess Lower!\nEnter your guess: ")
+            conn.sendall(b"Guess Lower!")
         elif guess < guessme:
-            conn.sendall(b"Guess Higher!\nEnter your guess:")
+            conn.sendall(b"Guess Higher!")
