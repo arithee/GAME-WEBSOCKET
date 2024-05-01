89916e059bb08c8efe483f6316f77ad8d6207f0a
arithee <trevoratienza53@gmail.com>
2024-05-01 21:45:23 +0800

    Added and synced difficulties

--- game_client.py
+++ game_client.py
@@ -8,36 +8,36 @@ def game():
         s = socket.socket()
         s.connect((host, port))
 
-        try:
-            # received the banner
-            data = s.recv(1024)
-            # print banner
-            print(data.decode().strip())
+        # received the banner
+        data = s.recv(1024)
+        # print banner
+        print(data.decode().strip())
 
-            while True:
-                # Get user input
-                user_input = input("").strip()
+        # Get difficulty choice from the user
+        difficulty_choice = input("Enter your choice (a/b/c): ").strip().lower()
+        s.sendall(difficulty_choice.encode())
 
-                s.sendall(user_input.encode())
-                reply = s.recv(1024).decode().strip()
-                print(reply)
-                # Repeats the game without disconnecting the client.
-                if "Correct" in reply:
-                    play_again = input("").strip().lower()
-                    s.sendall(play_again.encode())
-                    if play_again != "Y":
-                        s.close()
-                        return
-                    else:
-                        break  # Break out of the inner loop if the guess is correct
-                elif "Goodbye" in reply:
+        while True:
+            # Get user input
+            user_input = input("Enter your guess:").strip()
+
+            s.sendall(user_input.encode())
+            reply = s.recv(1024).decode().strip()
+            print(reply)
+
+            if "Correct" in reply:
+                play_again = input("Do you want to play again? (yes/no): ").strip().lower()
+                s.sendall(play_again.encode())
+                if play_again != "yes":
                     s.close()
                     return
+                else:
+                    break  # Break out of the inner loop if the guess is correct
+            elif "Goodbye" in reply:
+                s.close()
+                return
 
-        except ConnectionResetError:
-            print("The server closed the connection unexpectedly.")
-            s.close()
-            return
+        s.close()
 
 if __name__ == "__main__":
     game()
